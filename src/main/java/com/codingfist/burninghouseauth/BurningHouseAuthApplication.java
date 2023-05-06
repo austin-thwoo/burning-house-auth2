fe3f4062b641f4da784cc3d52d152c92c14b2ad5
austin-thwoo <austin-thwoo@naver.com>
2023-05-07 01:40:07 +0900

    first commit

---
+++ src/main/java/com/codingfist/burninghouseauth/BurningHouseAuthApplication.java
@@ -0,0 +1,13 @@
+package com.codingfist.burninghouseauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurningHouseAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurningHouseAuthApplication.class, args);
+    }
+
+}
